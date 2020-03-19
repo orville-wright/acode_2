5dddee8e803a7fecbc2962d6669bfe0a088d466f
dave@keateryx <orville.wright@usakiwi.com>
2020-03-19 11:36:46 -0700

    Update to lession #3

--- trade.py
+++ trade.py
@@ -33,30 +33,34 @@ pos_info = rx.list_positions()
 print ( " " )
 print ( f"---------------- Stock price info ------------------" )
 print ( " " )
-print ( f"RAW format data..." )
-print ( f"--------------------" )
+print ( f"Looking at the RAW format data we extracted..." )
+print ( f"---------------------------------------------------" )
 
 price_info = rx.get_barset(symbols='CODX', timeframe='1Min', limit=15)
 stock_bars = price_info['CODX']
-print ( f"Look inside ALL the data we extracted:\n {price_info}" )
+print ( f"{price_info}" )
 print ( " " )
-print ( f"Look at Price info for CODX:\n {stock_bars}" )
 
 #for k, v in price_info.items():
 #    print ( f"**KEY: {k}" )
-
-print ( " " )
-print ( f"Pretty &  nice formatted data..." )
-print ( f"---------------------------------" )
+print ( f"Some info about the data..." )
+print ( f"-----------------------------------" )
 my_keys = price_info.keys()
 list_of_my_keys = list(my_keys)
+print ( f"List of all stocks symbols held inside this data DICTionary: {list_of_my_keys}" )
+print ( f"How many stock symbols are inside this data dictionary: {len(list_of_my_keys)}" )
+print ( f"Dictionary KEY # 0 is for symbol: {list_of_my_keys[0]}" )
+print ( " " )
+print ( f"Looking at the raw data iside the {list_of_my_keys[0]} dataset..." )
+print ( f"{stock_bars}" )
 
-print ( f"**LIst of all DICT keys: {list_of_my_keys}" )
-print ( f"**Key 0 is: {list_of_my_keys[0]}" )
-#print ( price_info[list_of_my_keys[0]] )
+print ( " " )
+print ( f"Now look at the {list_of_my_keys[0]} data in a Pretty & Nicer format..." )
+print ( f"-----------------------------------------------------------------------" )
 data_list = price_info[list_of_my_keys[0]]
 for i in range(len(data_list)):
     print ( f"DATA ITEM #{i} is -> {data_list[i]}" )
+
 
 # Now access very specific parts of the data strcuitures
 num_of_datapoints = len(data_list)
@@ -64,7 +68,7 @@ first_open = stock_bars[0].o
 last_close = stock_bars[-1].c
 percent_change = round( ((last_close - first_open) / first_open * 100),4 )
 print ( " " )
-print ( f"Access specific data fields from the complex data structure..." )
+print ( f"Now ACCESS specific data fields from the complex data structure..." )
 print ( f"---------------------------------------------------------------" )
 print ( f"FIRST OPEN price: {first_open}" )
 print ( f"LAST CLOSE price: {last_close}" )
